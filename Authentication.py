4c97badd3146e95bb4c2f5d65a7e023c9cda7897
kalyanraj <kalyanraj3003@gmail.com>
2024-03-31 11:41:41 +0530

    Added Authentication sotware to the code

--- Authentication.py
+++ Authentication.py
@@ -2,13 +2,13 @@ import paho.mqtt.client as mqtt
 import time
 
 # Broker settings
-broker_address = "test.mosquitto.org"
+broker_address = "localhost"  # replace with broker address
 port = 1883  # default MQTT port
 username = "kalyan"
 password = "kalyan"
 
 # Callback functions
-def on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc):       # rc code denotes standards code of connection
     if rc == 0:
         print("Connected to MQTT broker")
     else:
@@ -20,6 +20,7 @@ def on_message(client, userdata, message):
 # Publisher
 def publish_message():
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1)
+    client.on_log=on_log
     client.username_pw_set(username, password)  # Set username and password
     client.connect(broker_address, port)
     client.loop_start()
@@ -39,6 +40,17 @@ def subscribe_to_topic():
     client.connect(broker_address, port)
     client.subscribe("test_topic")
     client.loop_forever()
+
+def on_log(client, userdata,level, buf):
+    if "Sending CONNECT" in buf:
+        if "u1" in buf:
+            print("Username is set.")
+        else:
+            print("Username is not set.")
+        if "p1" in buf:
+            print("Password is set.")
+        else:
+            print("Password is not set.")
 
 if __name__ == "__main__":
     # Run publisher and subscriber concurrently
